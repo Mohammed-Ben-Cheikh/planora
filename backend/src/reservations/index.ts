da5f2de41d7eb253143a42a98c8d91462fd4be17
mohammed-ben-cheikh <bencheikh.official@gmail.com>
2026-02-04 10:29:34 +0100

    feat(reservations): create index file for reservation exports

---
+++ backend/src/reservations/index.ts
@@ -0,0 +1,6 @@
+export * from './dto';
+export * from './entities/reservation.entity';
+export * from './reservations.controller';
+export * from './reservations.module';
+export * from './reservations.service';
+export * from './services/pdf.service';
