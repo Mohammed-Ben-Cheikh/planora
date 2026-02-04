a52f34df3060e1ece55d238e4b0a73fca7dec117
mohammed-ben-cheikh <bencheikh.official@gmail.com>
2026-02-04 10:43:39 +0100

    feat(users): define User entity with unique username and email fields, and include role and isActive properties

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,16 +1,42 @@
-import { Column, Entity, ObjectId, ObjectIdColumn } from 'typeorm';
+import { Exclude } from 'class-transformer';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ObjectId,
+  ObjectIdColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Role } from '../../common/enums/role.enum';
 
-@Entity()
+@Entity('users')
 export class User {
   @ObjectIdColumn()
   _id: ObjectId;
 
-  @Column()
+  @Column({ unique: true })
   username: string;
 
-  @Column()
-  email: String;
+  @Column({ unique: true })
+  email: string;
 
   @Column()
+  @Exclude()
   password: string;
+
+  @Column({ type: 'enum', enum: Role, default: Role.PARTICIPANT })
+  role: Role;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  constructor(partial: Partial<User>) {
+    Object.assign(this, partial);
+  }
 }
